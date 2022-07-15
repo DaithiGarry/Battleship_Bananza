2beed8bba2637c599abec46869115754c807a4d4
David Garry <daithigarry@hotmail.com>
2022-07-15 21:47:28 +0000

    class gameboard, gitignore info and imports

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class GameBoard:
+  def __init__(self, board):
+    self.board = board
+
+  def get_letters_to_numbers():
+    """ letters to numbers is used to identify the user column input to numbers on the grid"""
+    letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5}
+    return letters_to_numbers
+
+  def print_board(self):
+    """ defines the appearance of the userboard and labels"""
+      print("  A B C D E F")
+      print("+ - - - - - - +")
+      row_number = 1
+
+      for row in range(6):
+        """ defines the loop that runs through the board and registers the numbers and columns and the surrounding formatting"""
+        self.board.append(['-'] * 6)
+        number = 0
+
+      for row in range(6):
+        print(str(number + 1), end = '|')
+        for column in range(len(self.board[number])):
+          print(self.board[number][column], end= ' ')
+        print('| ')  
+        number += 1
+      print("+ - - - - - - +")
